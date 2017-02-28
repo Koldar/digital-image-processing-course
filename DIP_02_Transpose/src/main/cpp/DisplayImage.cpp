cc87e5bbb246224dc5fcddf53483b1e5626359b6
Koldar <mbono@unibs.it>
2017-02-28 09:38:40 +0100

    BASIC PROCESSING
    - added new exercises

--- DIP_02_Transpose/src/main/cpp/DisplayImage.cpp
+++ DIP_02_Transpose/src/main/cpp/DisplayImage.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
+#include <limits.h>
 
 using namespace cv;
 
@@ -13,11 +14,121 @@ typedef enum {
 	FE_VERTICAL
 } FlipEnum;
 
+static void addSaltAndPepperNoise(Mat& image);
+static void addWhiteNoise(Mat& image);
+static void addGaussianNoise(Mat& image);
+
 static void rotate90(Mat& image, RotationEnum re);
 static void flipImage(Mat& image, FlipEnum fe);
-static void crop(Mat& image, Rect2i cropRectangle);
+static void crop(Mat& image, Mat* output, const Rect2i& cropRectangle);
+static void negative(Mat& image, Mat* output);
+static void setImagePixelsTo(Mat& image, Vec3b color);
 
-static void crop(Mat& image, Rect2i cropRectangle) {
+static void addWhiteNoise(Mat& image) {
+	/**
+	 * The white noise
+	 */
+	randn(image, 0, 100000);
+}
+
+static void addGaussianNoise(Mat& image) {
+
+}
+
+static void addSaltAndPepperNoise(Mat& image) {
+
+	//static method: create a matrix all with zeros
+	Mat saltpepper_noise = Mat::zeros(image.rows, image.cols, CV_8U);
+
+	//now we fill saltpepper_noise matrix with values uniformly picked amont [0;255]
+	randu(saltpepper_noise,0,255);
+
+	/* ok, now this is interesting. <,>,<= and >= are overloaded. if you do "Mat < num" you will
+	 * obtain a new matrix always of pixel type 8U where each pixel is 0xFF if it is less than num, 0x00 otherwise.
+	 * See http://stackoverflow.com/a/9783817/1887602
+	 *
+	 * This allow us to automatically set binarize according to a condition our image! pretty cool!
+	 */
+	Mat black = saltpepper_noise < 30;
+	Mat white = saltpepper_noise > 225;
+
+	/*
+	 * SetTo seems pretty cool.
+	 * The second argument is a mask (aka a 8U matrix type).
+	 * We will set the pixel in "image" whose coordinates in "white" are "0xFF" to the first parameter (255 in this case).
+	 */
+	image.setTo(255, white);
+	image.setTo(0, black);
+}
+
+/**
+ * Set the all the pixel of the image to a certain color
+ *
+ * \attention
+ * 	\c image will be changed!
+ *
+ * @param[inout] image the image to alter
+ * @param[in] color the color each pixel will have
+ */
+static void setImagePixelsTo(Mat& image, Vec3b color) {
+	for (int y=0; y<image.rows; y++) {
+		for (int x=0; x<image.cols; x++) {
+				image.at<Vec3b>(y, x) = color;
+		}
+	}
+}
+
+/**
+ * compute the negative of an image
+ *
+ *
+ * <img src="https://3.bp.blogspot.com/-VGsbdrM3LTM/UOOSsvUkZoI/AAAAAAAAAek/i8AQnCmc_FY/s1600/imagenegative.png"/>
+ *
+ * @param[in] image the image to compute the negative on
+ * @param[out] output a non initialized ::Mat pointer
+ */
+static void negative(Mat& image, Mat* output) {
+	*output = Mat{image.rows, image.cols,  CV_8UC3};
+
+	for (int y=0; y<image.rows; y++) {
+		for (int x=0; x<image.cols; x++) {
+			for (int c=0; c<3; c++) {
+				output->at<Vec3b>(y, x).val[c] = UCHAR_MAX - image.at<Vec3b>(y,x).val[c];
+			}
+
+		}
+	}
+}
+
+/**
+ * Crop an image
+ *
+ * <img src="http://www.pyimagesearch.com/wp-content/uploads/2015/01/mouse-event-crop-tim.jpg"/>
+ *
+ * @param[in] image the image to crop on
+ * @param[out] a pointer to an empty ::Mat that will contain the cropped image
+ * @param[in] cropRectangle a rectangle that will be used to define the area cropped
+ */
+static void crop(const Mat& image, Mat* output, const Rect2i& cropRectangle) {
+	/*
+	 * CV_8UC3 indicates how we represents a single channel AND how many channel we will have.
+	 * 8U is how we represents a single channel:
+	 *  - 8U=8 bit unsigned char
+	 *  - 32F = 32 bit of floating-point
+	 *
+	 * The second sector of the constant represents  the number of channels. It is in the form of CX where X is a number:
+	 * - C3 are 3 channels;
+	 * - C4 are 4 channels;
+	 *
+	 * CV_8UC3 means we want an image with 3 channels each with 8 bit unsigned char
+	 */
+	*output = Mat{cropRectangle.width, cropRectangle.height,  CV_8UC3};
+
+	for (int y=0; y<cropRectangle.height; y++) {
+		for (int x=0; x<cropRectangle.width; x++) {
+			output->at<Vec3b>(y, x) = image.at<Vec3b>(y + cropRectangle.y, x + cropRectangle.x);
+		}
+	}
 
 }
 
@@ -50,6 +161,12 @@ static void flipImage(Mat& image, FlipEnum fe) {
 				break;
 			}
 			}
+			/*
+			 * To set or get a pixel of an image, you can use at method.
+			 * However, since an image may have several channels and they can be encoded in several ways, we need
+			 * a template to let C++ understand what exactly we want to get the pixels.
+			 * Very useful indeed
+			 */
 			image.at<Vec3b>(newY, newX) = tmp.at<Vec3b>(y, x);
 		}
 	}
@@ -122,7 +239,15 @@ int main(int argc, char** argv )
 	//rotate90(image, RE_ANTICLOCKWISE);
 
 	//flipImage(image, FE_HORIZONTAL);
-	flipImage(image, FE_VERTICAL);
+	//flipImage(image, FE_VERTICAL);
+
+	Mat output;
+	//crop(image, &output, Rect2i{100,100, 70, 50});
+	//negative(image, &output);
+	//setImagePixelsTo(image, Vec3b{120,0,0});
+
+	addWhiteNoise(image);
+	//addSaltAndPepperNoise(image);
 
 	namedWindow("Display Image", WINDOW_AUTOSIZE );
 	imshow("Display Image", image);
