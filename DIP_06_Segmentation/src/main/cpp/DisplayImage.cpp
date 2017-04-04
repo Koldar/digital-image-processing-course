2c0aec593eb97cc7c4ae3a33366d8269f9fa92ed
Koldar <mbono@unibs.it>
2017-04-04 09:27:08 +0200

    SEGMENTATION
    - implemented bu not rtested segmentation

--- DIP_06_Segmentation/src/main/cpp/DisplayImage.cpp
+++ DIP_06_Segmentation/src/main/cpp/DisplayImage.cpp
@@ -2,9 +2,13 @@
 #include <opencv2/opencv.hpp>
 #include <limits.h>
 #include <iostream>
+#include <queue>
+#include <stdexcept>
 
 using namespace cv;
 using namespace std;
+
+// OTSU ALGORITHM
 
 struct OtsuThresholdInfo {
 	float backgroundWeight;
@@ -17,15 +21,15 @@ struct OtsuThresholdInfo {
 };
 
 ostream& operator<<(ostream& os, const OtsuThresholdInfo& dt)
-	{
-		os << "w=" << dt.backgroundWeight << " m=" << dt.backgroundMean << " v=" << dt.backgroundVariance
-				<< " "
-				<< "w=" << dt.foregroundWeight << " m=" << dt.foregroundMean << " v=" << dt.foregroundVariance;
-	    return os;
-	}
+{
+	os << "w=" << dt.backgroundWeight << " m=" << dt.backgroundMean << " v=" << dt.backgroundVariance
+			<< " "
+			<< "w=" << dt.foregroundWeight << " m=" << dt.foregroundMean << " v=" << dt.foregroundVariance;
+	return os;
+}
 
 
-void computeHistogram(Mat& image, Mat& output) {
+static void computeHistogram(Mat& image, Mat& output) {
 	//COMPUTE HISTOGRAM
 	/// Establish the number of bins
 	int histSize = 256;
@@ -48,7 +52,7 @@ void applyOtsuThreshold(Mat& image, Mat& output, float threshold) {
 	}
 }
 
-void populateWeights(Mat& image, int threshold, OtsuThresholdInfo& output) {
+static void populateWeights(Mat& image, int threshold, OtsuThresholdInfo& output) {
 	double pixelNumber = image.rows * image.cols;
 
 	for (int y=0; y<image.rows; y++) {
@@ -65,7 +69,7 @@ void populateWeights(Mat& image, int threshold, OtsuThresholdInfo& output) {
 	output.backgroundWeight /= pixelNumber;
 }
 
-void populateMeans(Mat& image, Mat& histogram, int threshold, OtsuThresholdInfo& output) {
+static void populateMeans(Mat& image, Mat& histogram, int threshold, OtsuThresholdInfo& output) {
 
 	long sumForeground = 0;
 	long sumBackground = 0;
@@ -82,7 +86,7 @@ void populateMeans(Mat& image, Mat& histogram, int threshold, OtsuThresholdInfo&
 	output.backgroundMean /= sumBackground;
 }
 
-OtsuThresholdInfo populateVariance(Mat& image, Mat& histogram, float threshold, float meanBackground, float meanForeground, OtsuThresholdInfo& output) {
+static void populateVariance(Mat& image, Mat& histogram, float threshold, float meanBackground, float meanForeground, OtsuThresholdInfo& output) {
 
 	long sumForeground = 0;
 	long sumBackground = 0;
@@ -99,7 +103,7 @@ OtsuThresholdInfo populateVariance(Mat& image, Mat& histogram, float threshold, 
 	output.backgroundVariance /= sumBackground;
 }
 
-float populateClassVariance(OtsuThresholdInfo& oti) {
+static float populateClassVariance(OtsuThresholdInfo& oti) {
 	oti.classVariance = oti.backgroundWeight * oti.backgroundVariance + oti.foregroundWeight * oti.foregroundVariance;
 	return oti.classVariance;
 }
@@ -133,6 +137,130 @@ int computeOtsuThreshold(Mat& image) {
 	return minThreshold;
 }
 
+//REGION GROWING
+
+
+class Region {
+public:
+	int id;
+	float mean;
+	int size;
+	static Mat* pixelMapping;
+};
+
+static Point2i pickPixelWithoutRegion(vector<Region>& regions) {
+	for (int y=0; y<Region::pixelMapping->rows; y++) {
+		for (int x=0; x<Region::pixelMapping->cols; x++) {
+			//cout << "trying to pick up "<< x << ", " << y << " of region " << (int)(regions->at<uchar>(y, x)) << endl;
+			if (((int)Region::pixelMapping->at<uchar>(y, x)) == 0) {
+				return Point2i(x, y);
+			}
+		}
+	}
+	throw logic_error("no pixel without region");
+}
+
+bool shouldPointBeAddedInQueue(Point2i p, vector<Region>& regions) {
+	return (p.x >= 0) &&
+			(p.y >= 0) &&
+			(p.x < Region::pixelMapping->cols) &&
+			(p.y < Region::pixelMapping->rows) &&
+			(Region::pixelMapping->at<uchar>(p) == 0);
+}
+
+vector<Point2i> getNonVisitedNeighbours(vector<Region>& regions, Point2i p) {
+	vector<Point2i> retVal;
+
+	Point2i neighbour;
+	for (int y=-1; y<=+1; y++) {
+		for (int x=-1; x<=+1; x++) {
+			if (x==0 && y==0) {
+				continue;
+			}
+			neighbour = Point{p.x + x, p.y + y};
+			if (shouldPointBeAddedInQueue(neighbour, regions)) {
+				retVal.push_back(neighbour);
+			}
+		}
+	}
+	return retVal;
+}
+
+bool isPixelBelogingToRegion(Point2i p, uchar r, Mat& image, vector<Region>& regions) {
+	cout << image.at<uchar>(p) << " == " << (int)r << endl;
+	Region region;
+	for (vector<Region>::iterator it=regions.begin(); it!=regions.end(); ++it) {
+		if (it->id == (int)r) {
+			region = *it;
+		}
+	}
+	return abs(image.at<uchar>(p) - region.mean) < 50;
+}
+
+void growRegion(deque<Point2i>* openPixels, uchar currentRegionValue, Mat& image, vector<Region>& regions) {
+	Point2i p;
+	vector<Point2i> neighbourhood;
+
+	while (!openPixels->empty()) {
+		p = openPixels->front();
+		cout << "queue size " << openPixels->size() << " p: " << p << endl;
+		openPixels->pop_front();
+
+		if (isPixelBelogingToRegion(p, currentRegionValue, image, regions)) {
+			Region::pixelMapping->at<uchar>(p) = currentRegionValue;
+
+			//update region data
+
+
+			neighbourhood = getNonVisitedNeighbours(regions, p);
+			cout << "neighbourhood "<< neighbourhood << endl;
+			for (std::vector<Point2i>::iterator it=neighbourhood.begin(); it!=neighbourhood.end(); ++it) {
+				openPixels->push_back(*it);
+			}
+		}
+	}
+}
+
+Mat* computeSegmentationViaRegionGrowing(Mat& image) {
+	deque<Point2i>* openPixels = new deque<Point2i>;
+	Point2i p;
+	uchar currentRegionValue = 0;
+	vector<Point2i> neighbourhood;
+	vector<Region> regions;
+	Region::pixelMapping = new Mat{image.rows, image.cols, CV_8UC1};
+
+	Region::pixelMapping->setTo(0);
+
+	while (true) {
+		//pick a new pixel
+		try {
+			if (currentRegionValue == 255) {
+				throw logic_error("we used all the available regions!");
+			}
+			p = pickPixelWithoutRegion(regions);
+			cout << "picking up "<< p << " of region " << (int)(Region::pixelMapping->at<uchar>(p)) << endl;
+		} catch (logic_error e) {
+			cout << "out!" << endl;
+			//no pixel can be fetched
+			goto exit;
+		}
+		//the new pixel is a new region
+		currentRegionValue += 1;
+		Region::pixelMapping->at<uchar>(p) = currentRegionValue;
+		cout << "point "<< p << " is now of region " << (int)(Region::pixelMapping->at<uchar>(p)) << endl;
+		openPixels->push_back(p);
+		growRegion(openPixels, currentRegionValue, image, regions);
+	}
+
+	exit:
+	cout << "regions are " << (int)(currentRegionValue) << endl;
+	delete openPixels;
+	return regions;
+}
+
+
+// MAIN
+
 /**
  * Segmentation algorithm
  *
@@ -160,15 +288,21 @@ int main(int argc, char** argv )
 		return -1;
 	}
 
-	int threshold = computeOtsuThreshold(image);
-	Mat binarized(image.rows, image.cols, CV_8UC1);
-	applyOtsuThreshold(image, binarized, threshold);
-
+	//OTSU ALGORITHM
+	//	int threshold = computeOtsuThreshold(image);
+	//	Mat binarized(image.rows, image.cols, CV_8UC1);
+	//	applyOtsuThreshold(image, binarized, threshold);
 	/// Display
-	namedWindow("Otsu", CV_WINDOW_AUTOSIZE );
-	imshow("Otsu", binarized );
+	//		namedWindow("Otsu", CV_WINDOW_AUTOSIZE );
+	//		imshow("Otsu", binarized );
+	// waitKey(0);
 
+	//REGION GROWING
+	Mat* regions = computeSegmentationViaRegionGrowing(image);
+	namedWindow("Regions", CV_WINDOW_AUTOSIZE );
+	imshow("Regions", *regions);
 	waitKey(0);
+	delete regions;
 
 	return 0;
 }
